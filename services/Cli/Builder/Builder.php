91b0ed3f90f1ce32967dcef5d97d43fa637bd997
BOUGET Alexandre <abouget68@gmail.com>
2023-09-16 19:50:03 +0200

    continue builder start PSR

--- services/Cli/Builder/Builder.php
+++ services/Cli/Builder/Builder.php
@@ -10,14 +10,14 @@ require_once __DIR__ . '/IncludeList.php';
 
 class Builder
 {
-
-    
+    private string $_build_dir;
 
     public function build()
     {
         $getcwd = getcwd();
         $build_dir = $getcwd . '/builddir';
-        $this->create_buildir($build_dir);
+        $this->_build_dir = $build_dir;
+        $this->_createBuildir($build_dir);
         recurse_copy($getcwd . '/public', $build_dir . '/public');
 
         $includer = new Includer();
@@ -25,22 +25,65 @@ class Builder
         $get_services_classes = $includer->includeList($getcwd . '/services');
         $all_services = MakeClasses::makeClasses($get_services_classes);
 
-        $include_list = IncludeList();
+        $include_list = new IncludeList();
+        var_dump($all_services);
 
-        $this->
+        $this->_buildInterfaces($include_list);
+
+        $this->_buildServices($include_list, $all_services);
     }
 
-    private function create_buildir(string $build_dir)
+    /**
+     * Create the build directory
+     * 
+     * @return void
+     */
+    private function _createBuildir()
     {
 
-        if (is_dir($build_dir)) {
+        if (is_dir($this->_build_dir)) {
             // Remove the build folder
-            removeDir($build_dir);
+            removeDir($this->_build_dir);
         }
-        mkdir($build_dir);
-        mkdir($build_dir . '/services');
-        mkdir($build_dir . '/services/interfaces');
+        mkdir($this->_build_dir);
+        mkdir($this->_build_dir . '/services');
+        mkdir($this->_build_dir . '/services/interfaces');
     }
 
-    private function build_interfaces(IncludeList $include_list, array $all_services): 
+    /**
+     * Build All interfaces on Adebipe namespace
+     * 
+     * @param IncludeList $include_list The list of the interfaces
+     * 
+     * @return void
+     */
+    private function _buildInterfaces(IncludeList $include_list): void
+    {
+        foreach (get_declared_interfaces() as $interface) {
+            $interface = new ReflectionClass($interface);
+            if (!preg_match('/^Adebipe\\\\/', $interface->getName())) {
+                continue;
+            }
+            $file = $interface->getFileName();
+            $file_code = file_get_contents($file);
+            $file_path = $this->_build_dir . '/services/interfaces/' .
+                $interface->getShortName() . '.php';
+            file_put_contents($file_path, $file_code);
+            $include = substr($file_path, strlen($this->_build_dir . '/'));
+            $include_list->add($include);
+        }
+    }
+
+    /**
+     * Build all services
+     * 
+     * @param IncludeList $include_list The list of the interfaces
+     * @param array $all_services The list of all services
+     * 
+     * @return void
+     */
+    private function _buildServices(IncludeList $include_list, array $all_services): void
+    {
+        // TODO
+    }
 }
