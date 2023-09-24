1c18f7c116465e159766676d338ac9187a6ea758
BOUGET Alexandre <abouget68@gmail.com>
2023-09-24 20:44:59 +0200

    begin test

---
+++ tests/Logger.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Adebipe\Services\Core\Utils;
+
+use Adebipe\Services\Logger;
+use PHPUnit\Framework\TestCase;
+
+class LoggerTest extends TestCase
+{
+    public function testLogger()
+    {
+        $logger = new Logger();
+    }
+}
