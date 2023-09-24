6f7cf75ab3cec8e8b4a3276826b50ba9bf53bbec
BOUGET Alexandre <abouget68@gmail.com>
2023-09-24 21:19:03 +0200

    add phpunit to ci debug first test

--- tests/adebipe/services/core/utils/LoggerTest.php
+++ tests/adebipe/services/core/utils/LoggerTest.php
@@ -5,11 +5,13 @@ namespace Tests\Adebipe\Services\Core\Utils;
 use Adebipe\Services\Logger;
 use PHPUnit\Framework\TestCase;
 
+use function PHPUnit\Framework\assertTrue;
+
 class LoggerTest extends TestCase
 {
     public function testLogger()
     {
         $logger = new Logger();
-        
+        $this->assertTrue(true);
     }
 }
