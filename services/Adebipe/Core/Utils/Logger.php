e8aa9cd2b8e91feb14a27d76e7affc102f52c8ac
BOUGET Alexandre <abouget68@gmail.com>
2023-09-25 19:53:23 +0200

    test logger

--- services/Adebipe/Core/Utils/Logger.php
+++ services/Adebipe/Core/Utils/Logger.php
@@ -12,19 +12,20 @@ use Adebipe\Services\Interfaces\StarterServiceInterface;
  */
 class Logger implements StarterServiceInterface, RegisterServiceInterface
 {
-    private ErrorSenderInterface $_sender;
-    public $logTrace = array();
-
-
-    private $_logFile;
-    private $_loglevel;
-    private $_loglevels = [
+    private static array $_loglevels = [
         'DEBUG' => 0,
         'INFO' => 1,
         'WARNING' => 2,
         'ERROR' => 3,
         'CRITICAL' => 4
     ];
+
+    private ErrorSenderInterface $_sender;
+    public $logTrace = array();
+
+
+    private $_logFile;
+    private int $_loglevel;
 
 
     /**
@@ -35,9 +36,30 @@ class Logger implements StarterServiceInterface, RegisterServiceInterface
         if (!is_dir('logs')) {
             mkdir('logs');
         }
-        $this->_loglevel = getenv('LOG_LEVEL') ?? 1;
-        $this->_logFile = fopen('logs/' . date('Y-m-d-H-i-s') . '.log', 'wb');
+        $log_level = getenv('LOG_LEVEL');
+        if ($log_level === false) {
+            $log_level = 1;
+        }
+        $this->_loglevel = intval($log_level);
+        if ($this->_loglevel < 0 || $this->_loglevel > 4) {
+            throw new \Exception('Invalid log level');
+        }
+        if (!getenv('LOG_IN_FILE')) {
+            $this->_logFile = STDOUT;
+        } else {
+            $this->_logFile = fopen('logs/' . date('Y-m-d-H-i-s') . '.log', 'wb');
+        }
         $this->info('Starting Logger');
+    }
+
+    /**
+     * Get the log levels
+     *
+     * @return string
+     */
+    public function getLogLevels(): string
+    {
+        return array_keys(self::$_loglevels)[$this->_loglevel];
     }
 
     /**
@@ -116,10 +138,10 @@ class Logger implements StarterServiceInterface, RegisterServiceInterface
      */
     private function _log(string $type, string $message): void
     {
-        if (!isset($this->_loglevels[$type])) {
+        if (!isset(self::$_loglevels[$type])) {
             throw new \Exception('Invalid log type');
         }
-        if ($this->_loglevel > $this->_loglevels[$type]) {
+        if ($this->_loglevel > self::$_loglevels[$type]) {
             return;
         }
 
@@ -132,6 +154,9 @@ class Logger implements StarterServiceInterface, RegisterServiceInterface
             fflush(STDOUT);
         }
         $log = mb_convert_encoding($log, 'UTF-8');
+        if ($this->_logFile === STDOUT) {
+            return;
+        }
         fwrite($this->_logFile, $log);
         fflush($this->_logFile);
     }
