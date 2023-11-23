7cc44f7de31b0fc9a92a8d40282b0054924bdc67
BOUGET Alexandre <abouget68@gmail.com>
2023-11-23 15:41:05 +0100

    better includer

--- tests/include.php
+++ tests/include.php
@@ -1,7 +1,17 @@
 <?php
 
-foreach (scandir(__DIR__ . '/abstract') as $file) {
-    if ($file !== '.' && $file !== '..') {
-        require __DIR__ . '/abstract/' . $file;
+function includeIn($dir)
+{
+    foreach (scandir($dir) as $file) {
+        if ($file === '.' || $file === '..') {
+            continue;
+        }
+        if (is_dir($dir . '/' . $file)) {
+            includeIn($dir . '/' . $file);
+        } else {
+            require_once $dir . '/' . $file;
+        }
     }
 }
+
+includeIn(__DIR__ . '/abstract');
